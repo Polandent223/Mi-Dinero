01020e71d80bc74c06617bb0b8395d1130845811
Polandent223 <samuel.eliuzeq.223@gmail.com>
2026-09-09 17:25:13 -0300

    Parte 8: renovar caché tras endurecer restauración

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-dinero-p8-v1';
+const CACHE='mi-dinero-p8-v2';
 const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./icons/icon.svg','./src/startup-guard.js','./src/app.js','./src/db.js','./src/security.js','./src/sync.js','./src/portfolio.js','./src/wealth-ui.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
